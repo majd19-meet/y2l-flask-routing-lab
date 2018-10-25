c798a6b00d5192f5e08b57431ee9f231e7c68151
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:50:42 +0300

    instruments shop

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "You are home!"
+    return render_template('home.html')
+
+@app.route('/contact')
+def this():
+	return render_template('contact.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
